e7dd93a9f9b0aba9c269fda0500a56ab3d7191fa
jeffreyguezou <jeffreyrajcj@gmail.com>
2024-02-05 15:39:39 +0530

    Add new note and new tag functionality

---
+++ src/NewNote.tsx
@@ -0,0 +1,26 @@
+import { NoteData } from "./App";
+import NoteForm from "./NoteForm";
+import { Tag } from "./App";
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export default function NewNote({
+  onSubmit,
+  onAddTag,
+  availableTags,
+}: NewNoteProps) {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
+}
